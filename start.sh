322bedf7af20fe88e6273d228463420fe931c8c2
Dhavalkumar Vadgama <dvadgama@dmascot.co.uk>
2020-03-07 02:09:21 +0000

    update: aesthetic changes

--- start.sh
+++ start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 ES_HOST='localhost:9200'
+KIBANA_HOST='localhost:5601'
 
-if [ `docker images -q elasticsearch:SLACK_READY | wc -l` -eq 0 ]
+if [ ! -f .env ]
 then
-    echo "building custom ES image"
+ echo "Please Create .env file with suggested environment variables"
+ exit;
+fi
+
+TAG=`grep TAG .env | awk -F '=' '{print $2}'`
+
+if [ `docker images -q elasticsearch:${TAG} | wc -l` -eq 0 ]
+then
     docker-compose build
 fi 
 
@@ -16,11 +24,18 @@ do
     sleep 2
 done
 
-for file in `ls watcher/*.json`
+for file in `ls ./configs/watcher/*.json`
 do
     filename=`basename $file`
     alert_name=`basename $filename .json`
 
     echo "Adding/Updating ${alert_name}"
-    curl -H "Content-Type: application/json"  -d @./watcher/${filename} http://${ES_HOST}/_watcher/watch/${alert_name} > /dev/null 2>&1  
+    curl -H "Content-Type: application/json"  -d @./configs/watcher/${filename} http://${ES_HOST}/_watcher/watch/${alert_name} > /dev/null 2>&1  
+done
+
+until
+    curl -X GET http://${KIBANA_HOST}/ > /dev/null 2>&1
+do
+    echo "Waiting for Kibana to be online"
+    sleep 2
 done
